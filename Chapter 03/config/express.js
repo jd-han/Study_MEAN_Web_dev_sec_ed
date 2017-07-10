8032123b1d7a68f046db9869266f2768672121b6
Jd-han <woarey23@gmail.com>
2017-07-10 14:01:56 +0900

    chapter 3, page 69. tested. add module, process.env.NODE_ENV setting

--- Chapter 03/config/express.js
+++ Chapter 03/config/express.js
@@ -2,9 +2,26 @@
  * Created by human on 2017-07-07.
  */
 const express = require('express');
+const morgan = require('morgan');
+const compress = require('compression');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 module.exports = function () {
     const app = express();
+
+    if(process.env.NODE_ENV === 'development') {
+        app.use(morgan('dev'));
+    } else if (process.env.NODE_ENV === 'production'){
+        app.use(compress());
+    }
+
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(bodyParser.json());
+    app.use(methodOverride());
+
     require('../app/routes/index.server.routes.js')(app);
     return app;
 };
--- Chapter 03/server.js
+++ Chapter 03/server.js
@@ -1,6 +1,7 @@
 /**
  * Created by human on 2017-07-07.
  */
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const configureExpress = require('./config/express');
 
