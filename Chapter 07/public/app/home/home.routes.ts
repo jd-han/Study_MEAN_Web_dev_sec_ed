f8192555793927101cbc603edeb138d5cff2ffec
Jd-han <woarey23@gmail.com>
2017-07-19 21:03:49 +0900

    chapter 7. page   202. Creating the home module, Creating the home component, Configuring the home routes,  Refactoring the application module, Configuring the application routes

---
+++ Chapter 07/public/app/home/home.routes.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by human on 2017-07-19.
+ */
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
+
+export const HomeRoutes: Routes = [{
+    path: '',
+    component: HomeComponent,
+}];
